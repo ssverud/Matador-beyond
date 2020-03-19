c811eb76852fdd585c6f2bf8bc807fddebf6b196
Sunetb <sunetb@gmail.com>
2020-03-19 21:27:06 +0100

    Enum

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -3,7 +3,7 @@ import java.lang.reflect.Array;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 
-enum Types {
+enum FieldType {
     START,
     BLUE,
     PINK,
@@ -20,7 +20,7 @@ public class GameBoard {
     public GameBoard() {
         // String name, String type, int price, int priceToRent, int plotHouseExtensionPrice, int plotPledgePrice
         //blue
-        HousePlot Rødovrevej = new HousePlot("Rødovrevej", "BLUE", 1200, 50, 1000, 600);
+        HousePlot Rødovrevej = new HousePlot("Rødovrevej", FieldType.BLUE, 1200, 50, 1000, 600);
         HousePlot Hvidovrevej = new HousePlot("Hvidovrevej", "BLUE", 1200, 50, 1000, 600);
         //pink
         HousePlot Roskildevej = new HousePlot("Roskildevej", "PINK", 2000, 100, 1000, 1000);
--- src/HousePlot.java
+++ src/HousePlot.java
@@ -4,6 +4,7 @@ public class HousePlot {
 
     private String name;
     private String type;
+    private FieldType fieldType;
     private int price;
 
     private int priceToRent;
@@ -23,6 +24,18 @@ public class HousePlot {
         this.plotPledgePrice = plotPledgePrice;
     }
 
+    public HousePlot(String rødovrevej, FieldType type, int price, int priceToRent, int plotHouseExtensionPrice, int plotPledgePrice) {
+        this.name = name;
+        this.fieldType = type;
+        this.price = price;
+        this.priceToRent = priceToRent;
+        this.plotHouseExtensionPrice = plotHouseExtensionPrice;
+        this.plotPledgePrice = plotPledgePrice;
+        if(type == FieldType.CHANCEFIELD) System.out.println("Vi har et chancefelt!");
+        //Lækkert at man kan sammenligne med ==
+        // Det må man jo ellers ikke kan med strenge
+        //hvor man ellers skulle sige if(type.equals("CHANCEFIELD"))
+    }
 
 
     // If statements
