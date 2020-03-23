7230455803a9f2c0402e542e09891744fc909781
Stigafp <sstig.afp@gmail.com>
2020-03-23 21:26:17 +0100

    har tilføjet de forskellige housesplot typer til swwitch statement

--- src/GameField.java
+++ src/GameField.java
@@ -51,6 +51,28 @@ enum GameFieldType {
             return new HousePlot("Roskildevej", HousePlot.HousePlotType.PINK, 2000, 100, 1000, 1000);
         case "ValbyLanggade" :
             return new HousePlot("Valby Langgade", HousePlot.HousePlotType.PINK, 2000, 100, 1000, 1000);
+        case "Allegade" :
+            return new HousePlot("Allégade", HousePlot.HousePlotType.PINK, 2400, 150, 1000, 1200);
+        case "FrederiksbergAlle" :
+            return new HousePlot("Frederiksberg Alle", HousePlot.HousePlotType.GREEN, 2800, 200, 2000, 1400);
+        case "Bulowsvej":
+            return new HousePlot("Bülowsvej", HousePlot.HousePlotType.GREEN, 2800, 200, 2000, 1400);
+        case "GammelKongevej" :
+            return new HousePlot("Gammel Kongevej", HousePlot.HousePlotType.GREEN, 3200, 250, 2000, 1500);
+        case "Bernstorffsvej" :
+                return new HousePlot("Bernstorffsvej", HousePlot.HousePlotType.GREY, 3600, 300, 2000, 1800);
+        case "Hellerupvej" :
+            return new HousePlot("Hellerupvej", HousePlot.HousePlotType.GREY, 3600, 300, 2000, 1800);
+        case "Strandvej" :
+            return new HousePlot("Strandvej", HousePlot.HousePlotType.GREY, 4000, 350, 2000, 2000);
+        case "Trianglen" :
+            return new HousePlot("Trianglen", HousePlot.HousePlotType.RED, 4400, 350, 3000, 2200);
+        case "Oesterbrogade":
+            return new HousePlot("Østerbrogade", HousePlot.HousePlotType.RED, 4400, 350, 3000, 2200);
+        case "Groenningen" :
+            return new HousePlot("Grønningen", HousePlot.HousePlotType.RED, 4800, 400, 3000, 2400);
+        case "Bredgade" :
+            return new HousePlot("Bredgade", HousePlot.HousePlotType.WHITE, 5200, 450, 3000, 2600);
     }
     return new HousePlot("Roedovrevej", HousePlot.HousePlotType.BLUE, 1200, 50, 1000, 600);
     }
