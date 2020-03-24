84d16f5223993ac0815c9345d93456a4b86d45e6
jonsson0 <madsjonsson1997@gmail.com>
2020-03-24 11:45:05 +0100

    Arebejdet videre og git test

--- src/FerryField.java
+++ src/FerryField.java
@@ -3,18 +3,15 @@ public class FerryField {
     private String name;
     private int price;
     private int priceToRent;
-
-    private int plotHouseExtensionPrice;
-    private int plotPledgePrice;
+    private int pledgePrice;
 
     private boolean hasAllTypes;
 
 
-    public FerryField(String name, int price, int priceToRent, int plotHouseExtensionPrice, int plotPledgePrice) {
+    public FerryField(String name, int price, int priceToRent, int pledgePrice) {
         this.name = name;
         this.price = price;
         this.priceToRent = priceToRent;
-        this.plotHouseExtensionPrice = plotHouseExtensionPrice;
-        this.plotPledgePrice = plotPledgePrice;
+        this.pledgePrice = pledgePrice;
     }
 }
