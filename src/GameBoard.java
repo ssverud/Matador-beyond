fe5b24dcd388476649952f3c8d1a2f23595c2299
Stigafp <sstig.afp@gmail.com>
2020-04-10 13:12:21 +0200

    igang med at lave gameboardet om med added fields. Bare en test

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -17,55 +17,59 @@ public class GameBoard {
     public GameBoard() {
         TryYourLuck tryYourLuck = new TryYourLuck(); // TryYourLuck Instance
 
+
+
+        //grundpris, rentprice, huspris, pantsætning
+
         gameFields = new ArrayList<>(); // array of gameFields
         // Adding to our gameField list
         // ---> (For us only - Easier to create) : public GameField(int pos, GameFieldType gameFieldType, String fieldName)
         gameFields.add(new StartField(0, "START"));
-        gameFields.add(new PropertyField(1, "Rødovrevej"));
+        gameFields.add(new PropertyField(1, "Rødovrevej",1200, 50, 1000, 600));
         gameFields.add(new GameField(2, GameField.GameFieldType.CHANCEFIELD, "Prøv Lykken"));
-        gameFields.add(new GameField(3, GameField.GameFieldType.HOUSEFIELD, "Hvidovrevej"));
+        gameFields.add(new PropertyField(3, GameField.GameFieldType.HOUSEFIELD, "Hvidovrevej"));
         gameFields.add(new GameField(4, GameField.GameFieldType.TAXFIELD, "Betal indkomst skat"));
         gameFields.add(new GameField(5, GameField.GameFieldType.FERRYFIELD, "SFL-Færgene"));
-        gameFields.add(new GameField(6, GameField.GameFieldType.HOUSEFIELD, "Roskildevej"));
+        gameFields.add(new PropertyField(6, GameField.GameFieldType.HOUSEFIELD, "Roskildevej"));
         gameFields.add(new GameField(7, GameField.GameFieldType.CHANCEFIELD, "Prøv Lykken"));
-        gameFields.add(new GameField(8, GameField.GameFieldType.HOUSEFIELD, "Valby Langgade"));
-        gameFields.add(new GameField(9, GameField.GameFieldType.HOUSEFIELD, "Allégade"));
+        gameFields.add(new PropertyField(8, GameField.GameFieldType.HOUSEFIELD, "Valby Langgade"));
+        gameFields.add(new PropertyField(9, GameField.GameFieldType.HOUSEFIELD, "Allégade"));
 
 
         gameFields.add(new GameField(11, GameField.GameFieldType.VISITPRISON, "Besøg i fængsel"));
-        gameFields.add(new GameField(12, GameField.GameFieldType.HOUSEFIELD, "Frederiksberg Alle"));
+        gameFields.add(new PropertyField(12, GameField.GameFieldType.HOUSEFIELD, "Frederiksberg Alle"));
         gameFields.add(new GameField(13, GameField.GameFieldType.BREWERYFIELD, "Tuborg"));
-        gameFields.add(new GameField(14, GameField.GameFieldType.HOUSEFIELD, "Bülowsvej"));
-        gameFields.add(new GameField(15, GameField.GameFieldType.HOUSEFIELD, "Gammel Kongevej"));
+        gameFields.add(new PropertyField(14, GameField.GameFieldType.HOUSEFIELD, "Bülowsvej"));
+        gameFields.add(new PropertyField(15, GameField.GameFieldType.HOUSEFIELD, "Gammel Kongevej"));
         gameFields.add(new GameField(16, GameField.GameFieldType.FERRYFIELD, "DSB-Rederierne: Kalundborg-Århus"));
-        gameFields.add(new GameField(17, GameField.GameFieldType.HOUSEFIELD, "Bernstorffsvej"));
+        gameFields.add(new PropertyField(17, GameField.GameFieldType.HOUSEFIELD, "Bernstorffsvej"));
         gameFields.add(new GameField(18, GameField.GameFieldType.CHANCEFIELD, "Prøv Lykken"));
-        gameFields.add(new GameField(19, GameField.GameFieldType.HOUSEFIELD, "Hellerupvej"));
-        gameFields.add(new GameField(20, GameField.GameFieldType.HOUSEFIELD, "Strandvej"));
+        gameFields.add(new PropertyField(19, GameField.GameFieldType.HOUSEFIELD, "Hellerupvej"));
+        gameFields.add(new PropertyField(20, GameField.GameFieldType.HOUSEFIELD, "Strandvej"));
 
 
         gameFields.add(new GameField(21, GameField.GameFieldType.PARKINGFIELD, "Parkering"));
-        gameFields.add(new GameField(22, GameField.GameFieldType.HOUSEFIELD, "Trianglen"));
+        gameFields.add(new PropertyField(22, GameField.GameFieldType.HOUSEFIELD, "Trianglen"));
         gameFields.add(new GameField(23, GameField.GameFieldType.CHANCEFIELD, "Prøv Lykken"));
-        gameFields.add(new GameField(24, GameField.GameFieldType.HOUSEFIELD, "Østerbrogade"));
-        gameFields.add(new GameField(25, GameField.GameFieldType.HOUSEFIELD, "Grønningen"));
+        gameFields.add(new PropertyField(24, GameField.GameFieldType.HOUSEFIELD, "Østerbrogade"));
+        gameFields.add(new PropertyField(25, GameField.GameFieldType.HOUSEFIELD, "Grønningen"));
         gameFields.add(new GameField(26, GameField.GameFieldType.FERRYFIELD, "DFDS-Seaways"));
-        gameFields.add(new GameField(27, GameField.GameFieldType.HOUSEFIELD, "Bredgade"));
-        gameFields.add(new GameField(28, GameField.GameFieldType.HOUSEFIELD, "Kongens Nytorv"));
+        gameFields.add(new PropertyField(27, GameField.GameFieldType.HOUSEFIELD, "Bredgade"));
+        gameFields.add(new PropertyField(28, GameField.GameFieldType.HOUSEFIELD, "Kongens Nytorv"));
         gameFields.add(new GameField(29, GameField.GameFieldType.BREWERYFIELD, "Coca cola"));
-        gameFields.add(new GameField(30, GameField.GameFieldType.HOUSEFIELD, "Østergade"));
+        gameFields.add(new PropertyField(30, GameField.GameFieldType.HOUSEFIELD, "Østergade"));
 
 
         gameFields.add(new GameField(31, GameField.GameFieldType.PRISONFIELD, "De fængsles"));
-        gameFields.add(new GameField(32, GameField.GameFieldType.HOUSEFIELD, "Amagertorv"));
-        gameFields.add(new GameField(33, GameField.GameFieldType.HOUSEFIELD, "Vimmelskaftet"));
+        gameFields.add(new PropertyField(32, GameField.GameFieldType.HOUSEFIELD, "Amagertorv"));
+        gameFields.add(new PropertyField(33, GameField.GameFieldType.HOUSEFIELD, "Vimmelskaftet"));
         gameFields.add(new GameField(34, GameField.GameFieldType.CHANCEFIELD, "Prøv Lykken"));
-        gameFields.add(new GameField(35, GameField.GameFieldType.HOUSEFIELD, "Nygade"));
+        gameFields.add(new PropertyField(35, GameField.GameFieldType.HOUSEFIELD, "Nygade"));
         gameFields.add(new GameField(36, GameField.GameFieldType.FERRYFIELD, "DSB-Rederierne: Halsskov-Knudshoved"));
         gameFields.add(new GameField(37, GameField.GameFieldType.CHANCEFIELD, "Prøv Lykken"));
-        gameFields.add(new GameField(38, GameField.GameFieldType.HOUSEFIELD, "Frederiksborggade"));
+        gameFields.add(new PropertyField(38, GameField.GameFieldType.HOUSEFIELD, "Frederiksborggade"));
         gameFields.add(new GameField(39, GameField.GameFieldType.TAXFIELD, "Ekstraordinær statsskat"));
-        gameFields.add(new GameField(40, GameField.GameFieldType.HOUSEFIELD, "Rådhuspladsen"));
+        gameFields.add(new PropertyField(40, GameField.GameFieldType.HOUSEFIELD, "Rådhuspladsen"));
 
     }
 
