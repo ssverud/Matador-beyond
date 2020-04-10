c4e2cac1d1116ed397831826d4284be9ef2d4030
Mads Køie Nielsen <madskoie@hotmail.com>
2020-04-10 12:53:28 +0200

    Addet rollcard metode i chancefield med ekstra

--- src/ChanceField.java
+++ src/ChanceField.java
@@ -6,7 +6,7 @@ public class ChanceField extends GameField {
     Random random = new Random();
     int pos;
     ArrayList cardsOfChance = new ArrayList<CardOfChance>();
-
+// hej
 
     public ChanceField() {
 
