a07e95c424ea3b77889b68518b2c4f65b1f11bf4
Mads Køie Nielsen <madskoie@hotmail.com>
2020-04-10 12:48:55 +0200

    Addet rollcard metode i chancefield

--- src/ChanceField.java
+++ src/ChanceField.java
@@ -1,2 +1,23 @@
-public class ChanceField {
-}
+import java.util.ArrayList;
+import java.util.Random;
+
+public class ChanceField extends GameField {
+    TryYourLuck tryYourLuck = new TryYourLuck();
+    Random random = new Random();
+    int pos;
+    ArrayList cardsOfChance = new ArrayList<CardOfChance>();
+
+
+    public ChanceField() {
+
+    }
+
+        public void rollCard(){
+            int roll;
+            roll = random.nextInt(tryYourLuck.cardsOfChance.size());
+            System.out.println(tryYourLuck.cardsOfChance.get(roll));
+
+        }
+
+
+    }
