125894e6f369498002d925101cf5eb56757b8768
Stigafp <sstig.afp@gmail.com>
2020-05-04 14:34:34 +0200

    oprettet test class af cardOfChanceDeck

---
+++ Test/TestingCardOfChanceDeck.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestingCardOfChanceDeck {
+
+    /*
+    @Test
+    public void Test_
+    
+     */
+}
