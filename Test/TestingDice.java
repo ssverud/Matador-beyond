1ef65920df6071dd8561aec555f17ae630c88a5f
Stigafp <sstig.afp@gmail.com>
2020-05-04 11:11:27 +0200

    Oprettet test mappe for tests og prøver at oprette test for matador dice

---
+++ Test/TestingDice.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+public class TestingDice {
+
+    @Test
+    public void Test_DiceRoll(){
+
+        //Arrange - Write variables for the expected result
+
+
+        //Act - invoke the method that needs testing with the variables
+
+        //Assert - compare whether the expected result matches the actual result
+
+
+
+    }
+}
